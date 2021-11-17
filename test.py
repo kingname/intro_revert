3f3dd1dea750473e298651b33210756fd729250d
qiankun xie <qiankun.xie@newsbreak.com>
2021-11-17 17:25:40 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+doc1 = '我们提交了一次代码，并发起了PR。管理员合并了这个PR'
+doc2 = '突然我发现这个代码里面有隐藏的bug，于是我发起了一次revert'
+doc3 = 'revert本质上也是一次PR，只不过它的修改跟上一个PR是反的'
+doc4 = '我在上次代码的基础上又做了一些修改。此时应该怎么提交？'
+
+print(doc1)
+print(doc2)
+print(doc3)
+print(doc4)
